2222b43085b28b8c445ee8b553c68967dbe8b61b
fidelisprasetyo <fidelisprasetyo@gmail.com>
2023-09-17 23:37:48 -0700

    implemented hw1 features

--- src/GUI.java
+++ src/GUI.java
@@ -5,57 +5,62 @@ import java.awt.event.ActionListener;
 
 public class GUI extends JFrame implements ActionListener {
 
-    private JLabel inputImageIcon;
-    private JLabel outputImageIcon;
     private JButton browseButton = new JButton("Browse");
-    private JButton applyButton = new JButton("Apply");
+    private JButton executeButton = new JButton("Execute");
     private JButton saveButton = new JButton("Save Output");
+    private JButton undoButton = new JButton("Undo All");
     static JToolBar toolBar;
     static JComboBox comboBox;
     private ImageHandler imageHandler;
+    JFrame frame = new JFrame("Digital Image Processing");
 
     public GUI() {
 
         // prepare GUI frame
-        JFrame frame = new JFrame("Digital Image Processing");
         frame.setSize(1600, 900);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
         // prepare toolbar and combobox
         toolBar = new JToolBar();
-        comboBox = new JComboBox(new String[] {"Convert to PGM", "Scale spatial resolution", "Change level resolution"});
+        comboBox = new JComboBox(new String[] {"Select an action", "Convert to PGM", "Change spatial resolution", "Change gray level resolution"});
 
         // prepare image panels
-        JPanel inputPanel = new JPanel();
-        JPanel outputPanel = new JPanel();
-        inputImageIcon = new JLabel();
-        outputImageIcon = new JLabel();
-        inputPanel.add(inputImageIcon);
-        outputPanel.add(outputImageIcon);
+        JLabel leftImage = new JLabel();
+        JLabel rightImage = new JLabel();
+        JScrollPane inputScrollPane = new JScrollPane(leftImage);
+        JScrollPane outputScrollPane = new JScrollPane(rightImage);
 
         // put input and output panels into a container
         JPanel imagePanelContainer = new JPanel(new GridLayout(1,2));
-        imagePanelContainer.add(inputPanel);
-        imagePanelContainer.add(outputPanel);
+        imagePanelContainer.add(inputScrollPane);
+        imagePanelContainer.add(outputScrollPane);
+
+//        // Log bar
+//        JPanel logPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+//        JLabel logText = new JLabel();
+//        logPanel.add(logText);
 
         // place buttons and combobox into toolbar
         toolBar.add(browseButton);
-        toolBar.add(applyButton);
+        toolBar.add(executeButton);
         toolBar.add(saveButton);
         toolBar.add(comboBox);
+        toolBar.add(undoButton);
 
-        // place button and container on the frame
+        // place gui components
         frame.add(imagePanelContainer, BorderLayout.CENTER);
         frame.add(toolBar, BorderLayout.NORTH);
+        //frame.add(logPanel, BorderLayout.SOUTH);
 
         // initiate imageHandler
-        imageHandler = new ImageHandler(inputImageIcon, outputImageIcon);
+        imageHandler = new ImageHandler(leftImage, rightImage);
 
         // button actionListeners
         comboBox.addActionListener(new ComboBoxActionListener());
         browseButton.addActionListener(new BrowseBtnActionListener());
         saveButton.addActionListener(new SaveBtnActionListener());
+        undoButton.addActionListener(new UndoBtnActionListener());
         frame.setVisible(true);
     }
 
@@ -68,19 +73,31 @@ public class GUI extends JFrame implements ActionListener {
         // not used
     }
 
-    private class ComboBoxActionListener implements  ActionListener {
+    private class UndoBtnActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            imageHandler.revertToOriginal();
+        }
+    }
+
+    private class ComboBoxActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             int selectedOption = (int) comboBox.getSelectedIndex();
+            ActionListener[] listeners = executeButton.getActionListeners();
+            for(ActionListener listener : listeners) {
+                executeButton.removeActionListener(listener);
+            }
             if (selectedOption == 0) {
-                // TODO: convert to PGM option
-                applyButton.addActionListener(new ConvertToPGMActionListener());
+                // do nothing
             } else if (selectedOption == 1) {
-                // TODO: Scale spatial resolution option
+                // convert to PGM
+                executeButton.addActionListener(new ConvertToPGMActionListener());
             } else if (selectedOption == 2) {
-                // TODO: Change level resolution option
-            } else {
-
+                // Scale spatial resolution option
+                executeButton.addActionListener(new SpatialResolutionActionListener());
+            } else if (selectedOption == 3) {
+                executeButton.addActionListener(new GrayResolutionActionListener());
             }
         }
     }
@@ -95,14 +112,190 @@ public class GUI extends JFrame implements ActionListener {
     private class ConvertToPGMActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            imageHandler.convertToPGM();
+            if(imageHandler.isInputNull()) {
+                JOptionPane.showMessageDialog(null, "No input file!", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                imageHandler.convertToPGM();
+            }
         }
     }
 
+    private class SpatialResolutionActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(imageHandler.isInputNull()) {
+                JOptionPane.showMessageDialog(null, "No input file!", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                int inputWidth = imageHandler.getInputImage().getWidth();
+                int inputHeight = imageHandler.getInputImage().getHeight();
+
+                JDialog popUpDialog = new JDialog(frame, "Change spatial resolution", true);
+                popUpDialog.setLayout(new GridLayout(4, 1));
+
+                JPanel textPanel = new JPanel();
+                JLabel widthText = new JLabel("Width");
+                JLabel heightText = new JLabel("Height");
+                textPanel.add(widthText);
+                textPanel.add(heightText);
+
+                JPanel spatResPanel = new JPanel();
+                JTextField widthTextField = new JTextField(String.valueOf(inputWidth), 4);
+                JTextField heightTextField = new JTextField(String.valueOf(inputHeight), 4);
+                spatResPanel.add(widthTextField);
+                spatResPanel.add(heightTextField);
+
+                JPanel comboBoxPanel = new JPanel();
+                String[] options = {"Select an algorithm", "Nearest Neighbor", "Linear", "Bilinear"};
+                JComboBox<String> comboBox = new JComboBox<>(options);
+                comboBoxPanel.add(comboBox);
+
+                JPanel buttonPanel = new JPanel();
+                JButton okButton = new JButton("Ok");
+                JButton cancelButton = new JButton("Cancel");
+                buttonPanel.add(okButton);
+                buttonPanel.add(cancelButton);
+
+                popUpDialog.add(textPanel);
+                popUpDialog.add(spatResPanel);
+                popUpDialog.add(comboBoxPanel);
+                popUpDialog.add(buttonPanel);
+
+                cancelButton.addActionListener(e17 -> popUpDialog.dispose());
+
+                widthTextField.addActionListener(e16 -> {
+                    String widthInput = widthTextField.getText();
+                    try {
+                        int curWidth = inputWidth;
+                        int newWidth = Integer.parseInt(widthInput);
+
+                        int newHeight = inputHeight * newWidth / curWidth;
+                        heightTextField.setText(Integer.toString(newHeight));
+
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Please enter an integer!", "Error", JOptionPane.ERROR_MESSAGE);
+                        widthTextField.setText(Integer.toString(inputWidth));
+                    }
+                });
+
+                widthTextField.addActionListener(e16 -> {
+                    String widthInput = widthTextField.getText();
+                    try {
+                        int curWidth = inputWidth;
+                        int newWidth = Integer.parseInt(widthInput);
+
+                        int newHeight = inputHeight * newWidth / curWidth;
+                        heightTextField.setText(Integer.toString(newHeight));
+
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Please enter an integer!", "Error", JOptionPane.ERROR_MESSAGE);
+                        widthTextField.setText(Integer.toString(inputWidth));
+                    }
+                });
+
+                heightTextField.addActionListener(e15 -> {
+                    String heightInput = heightTextField.getText();
+                    try {
+                        int curHeight = inputHeight;
+                        int newHeight = Integer.parseInt(heightInput);
+
+                        int newWidth = inputWidth * newHeight / curHeight;
+                        widthTextField.setText(Integer.toString(newWidth));
+
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Please enter an integer!", "Error", JOptionPane.ERROR_MESSAGE);
+                        heightTextField.setText(Integer.toString(inputHeight));
+                    }
+                });
+
+                comboBox.addActionListener(e14 -> {
+                    int srOption = comboBox.getSelectedIndex();
+
+                    ActionListener[] listeners = okButton.getActionListeners();
+                    for(ActionListener listener : listeners) {
+                        okButton.removeActionListener(listener);
+                    }
+
+                    if (srOption == 0) {
+                        // do nothing
+                    }
+                    else if (srOption == 1) {        // Nearest Neighbor
+                        okButton.addActionListener(e13 -> {
+                            int newWidth = Integer.parseInt(widthTextField.getText());
+                            int newHeight = Integer.parseInt(heightTextField.getText());
+                            imageHandler.nearestNeighbor(newWidth, newHeight);
+                            popUpDialog.dispose();
+                        });
+                    } else if (srOption == 2) {     // Linear Interpolation
+                        okButton.addActionListener(e12 -> {
+                            int newWidth = Integer.parseInt(widthTextField.getText());
+                            int newHeight = Integer.parseInt(heightTextField.getText());
+                            imageHandler.imageInterpolation(newWidth, newHeight, ImageHandler.LINEAR);
+                            popUpDialog.dispose();
+                        });
+                    } else {            // Bilinear Interpolation
+                        okButton.addActionListener(e1 -> {
+                            int newWidth = Integer.parseInt(widthTextField.getText());
+                            int newHeight = Integer.parseInt(heightTextField.getText());
+                            imageHandler.imageInterpolation(newWidth, newHeight, ImageHandler.BILINEAR);
+                            popUpDialog.dispose();
+                        });
+                    }
+                });
+
+                popUpDialog.setSize(300,200);
+                popUpDialog.setLocationRelativeTo(frame);
+                popUpDialog.setVisible(true);
+            }
+        }
+    }
+
+    private class GrayResolutionActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(imageHandler.isInputNull()) {
+                JOptionPane.showMessageDialog(null, "No input file!", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                int inputGrayDepth = imageHandler.getGrayDepth(imageHandler.getInputImage());
+                JDialog popUpDialog = new JDialog(frame, "Change gray level resolution ", true);
+                popUpDialog.setLayout(new GridLayout(3, 1));
+
+                JPanel textPanel = new JPanel();
+                JPanel textFieldPanel = new JPanel();
+                JLabel textLabel = new JLabel("Gray resolution");
+                JLabel bitTextLabel = new JLabel("bits");
+                JTextField grayTextField = new JTextField(String.valueOf(inputGrayDepth), 2);
+                textPanel.add(textLabel);
+                textFieldPanel.add(grayTextField);
+                textFieldPanel.add(bitTextLabel);
+
+                JPanel buttonPanel = new JPanel();
+                JButton okButton = new JButton("Ok");
+                JButton cancelButton = new JButton("Cancel");
+                buttonPanel.add(okButton);
+                buttonPanel.add(cancelButton);
+
+                popUpDialog.add(textPanel);
+                popUpDialog.add(textFieldPanel);
+                popUpDialog.add(buttonPanel);
+
+                cancelButton.addActionListener(e17 -> popUpDialog.dispose());
+                okButton.addActionListener(e1 -> {
+                    String input = grayTextField.getText();
+                    int newDepth = Integer.parseInt(input);
+                    imageHandler.changeGrayLevel(newDepth);
+                    popUpDialog.dispose();
+                });
+
+                popUpDialog.setSize(300,200);
+                popUpDialog.setLocationRelativeTo(frame);
+                popUpDialog.setVisible(true);
+            }
+        }
+    }
+    // TODO!!!
     private class SaveBtnActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            ImageHandler imageHandler = new ImageHandler(inputImageIcon, outputImageIcon);
             // TODO
         }
     }
--- src/ImageHandler.java
+++ src/ImageHandler.java
@@ -2,20 +2,36 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.Raster;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 class ImageHandler {
-    private JLabel inputImageIcon;
-    private JLabel outputImageIcon;
+
+    public static final int LEFT_IMAGE = 0;
+    public static final int RIGHT_IMAGE = 1;
+    public static final int LINEAR = 0;
+    public static final int BILINEAR = 1;
+    private BufferedImage originalImage;
     private BufferedImage inputImage;
     private BufferedImage outputImage;
+    private JLabel leftImage;
+    private JLabel rightImage;
 
-    public ImageHandler(JLabel inputImageIcon, JLabel outputImageIcon) {
-        this.inputImageIcon = inputImageIcon;
-        this.outputImageIcon = outputImageIcon;
+    public ImageHandler(JLabel leftImage, JLabel rightImage) {
+        this.leftImage = leftImage;
+        this.rightImage = rightImage;
+    }
+
+    public boolean isInputNull() {
+        return (inputImage == null);
+    }
+
+    public BufferedImage getInputImage() {
+        return inputImage;
     }
 
     public void openImage() {
@@ -28,13 +44,13 @@ class ImageHandler {
         if(returnValue == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             try {
-                inputImage = ImageIO.read(selectedFile);
-                if (inputImage != null) {
-                    inputImageIcon.setIcon(new ImageIcon(inputImage));
+                originalImage = ImageIO.read(selectedFile);
+                if (originalImage != null) {
+                    updateDisplayedImage(originalImage, LEFT_IMAGE);
+                    inputImage = originalImage;
                 } else {
                     JOptionPane.showMessageDialog(null, "Failed to open file", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(null, "Error! Exception code: " + e, "Error", JOptionPane.ERROR_MESSAGE);
@@ -42,19 +58,58 @@ class ImageHandler {
         }
     }
 
+    public void revertToOriginal() {
+        inputImage = originalImage;
+        updateDisplayedImage(inputImage, RIGHT_IMAGE);
+    }
+
+    private void updateDisplayedImage(BufferedImage image, int leftOrRight) {
+        if (leftOrRight == LEFT_IMAGE) {
+            leftImage.setIcon(new ImageIcon(image));
+        } else if (leftOrRight == RIGHT_IMAGE) {
+            rightImage.setIcon(new ImageIcon(image));
+        } else {
+            leftImage.setIcon(new ImageIcon(image)); // default
+        }
+    }
+
+    public int getGrayDepth(BufferedImage img) {
+        Raster raster = img.getData();
+        int grayDepth = raster.getSampleModel().getSampleSize(0);
+        return grayDepth;
+    }
+
     public void convertToPGM() {
-        if (inputImage != null) {
-            try {
-                int height = inputImage.getHeight();
-                int width = inputImage.getWidth();
+        try {
+            int grayDepth = getGrayDepth(inputImage);
+            int maxGrayLevel = (int) Math.pow(2, grayDepth) - 1;
+            int width = inputImage.getWidth();
+            int height = inputImage.getHeight();
 
-                File pgmImg = new File("./output/test.pgm");
-                BufferedWriter writer = new BufferedWriter(new FileWriter(pgmImg));
+            JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("PGM Files", "pgm");
 
+            fileChooser.setDialogTitle("Save PGM File");
+            fileChooser.setFileFilter(filter);
+
+            int userSelection = fileChooser.showSaveDialog(null);
+
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                String filePath = selectedFile.getPath();
+
+                if(!filePath.endsWith(".pgm")) {
+                    filePath += ".pgm";
+                    selectedFile = new File(filePath);
+                }
+
+                BufferedWriter writer = new BufferedWriter(new FileWriter(selectedFile));
+
+                // write the values into pgm file
                 writer.write("P2\n"); // ASCII format
                 writer.write("# Converted Image to PGM\n");    // comment
                 writer.write(width + " " + height + "\n");  // number of rows and columns
-                writer.write("255\n");
+                writer.write(maxGrayLevel + "\n");  // maximum gray level
 
                 for(int y = 0; y < height; y++) {
                     for(int x = 0; x < width; x++) {
@@ -65,17 +120,114 @@ class ImageHandler {
                     writer.write('\n');
                 }
                 writer.close();
-                JOptionPane.showMessageDialog(null, "File saved to /output directory", "Success!", JOptionPane.INFORMATION_MESSAGE);
-            } catch (IOException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Error! Exception code: " + e, "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "File saved successfully", "Success!", JOptionPane.INFORMATION_MESSAGE);
             }
-        } else {
-            JOptionPane.showMessageDialog(null, "No input file!", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error! Exception code: " + e, "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    public void saveOutputImage() {
+    public void nearestNeighbor(int newWidth, int newHeight) {
+        int inputWidth = inputImage.getWidth();
+        int inputHeight = inputImage.getHeight();
+        outputImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
 
+        if (newWidth > inputWidth) {        // zoom image
+            double ratio = newWidth/ (double)inputWidth;
+            for(int y = 0; y < newHeight; y++) {
+                for(int x = 0; x < newWidth; x++) {
+                    int X = (int) Math.floor(x/ratio);
+                    int Y = (int) Math.floor(y/ratio);
+
+                    int g = inputImage.getRGB(X,Y);
+                    outputImage.setRGB(x,y,g);
+                }
+            }
+        } else {    // shrink image
+            double ratio = inputWidth/ (double)newWidth;
+            for(int y = 0; y < newHeight; y++) {
+                for(int x = 0; x < newWidth; x++) {
+                    int X = (int) Math.floor(x*ratio);
+                    int Y = (int) Math.floor(y*ratio);
+                    int g = inputImage.getRGB(X,Y);
+                    outputImage.setRGB(x,y,g);
+                }
+            }
+        }
+        updateDisplayedImage(outputImage, RIGHT_IMAGE);
+        inputImage = outputImage; // update the input
+    }
+
+    public void imageInterpolation(int newWidth, int newHeight, int method) {
+        int inputWidth = inputImage.getWidth();
+        int inputHeight = inputImage.getHeight();
+        outputImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+
+        double ratio = (double)inputWidth/ newWidth;
+
+        for(int y = 0; y < newHeight; y++) {
+            for(int x = 0; x < newWidth; x++) {
+                double X = x * ratio;
+                double Y = y * ratio;
+
+                int x0 = (int) Math.floor(X);
+                int x1 = (int) Math.ceil(X);
+                int y0 = (int) Math.floor(Y);
+                int y1 = (int) Math.ceil(Y);
+
+                // bound check
+                x0 = Math.max(x0, 0);
+                x1 = Math.min(x1, inputWidth - 1);
+                y0 = Math.max(y0, 0);
+                y1 = Math.min(y1, inputHeight -1);
+
+                // get gray values
+                int F00 = inputImage.getRGB(x0,y0) & 0xFF;
+                int F01 = inputImage.getRGB(x0,y1) & 0xFF;
+                int F10 = inputImage.getRGB(x1,y0) & 0xFF;
+                int F11 = inputImage.getRGB(x1,y1) & 0xFF;
+
+                double a = X - x0;
+                double b = Y - y0;
+                double p;
+
+                if(method == LINEAR) {
+                    p = (F00 * ( 1 - a )) + (F11 * a);  // Linear interpolation
+                } else {
+                    p = ((1 - a) * (1 - b) * F00) + (a * (1 - b) * F10) + (a * b * F11) + ((1 - a) * b * F01);  // Bilinear interpolation
+                }
+
+                int pixel = (int) Math.min(255, Math.max(0, p));
+                int rgb = (pixel << 16) | (pixel << 8) | pixel;   // construct the rgb value
+
+                outputImage.setRGB(x,y,rgb);
+            }
+        }
+        updateDisplayedImage(outputImage, RIGHT_IMAGE);
+        inputImage = outputImage; // update the input
+    }
+
+    public void changeGrayLevel(int newDepth) {
+        int inputDepth = getGrayDepth(inputImage);
+        int inputLevel = (int) Math.pow(2, inputDepth);
+        int targetLevel = (int) Math.pow(2, newDepth);
+
+        int ratio = inputLevel/ targetLevel;
+
+        int width = inputImage.getWidth();
+        int height = inputImage.getHeight();
+
+        outputImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                int rgb = inputImage.getRGB(x,y) & 0xFF;
+                int newPixelValue = (int) Math.floor((double)rgb/inputLevel * targetLevel) * ratio;
+                int g = (newPixelValue << 16) | (newPixelValue << 8) | newPixelValue;
+                outputImage.setRGB(x,y,g);
+            }
+        }
+        updateDisplayedImage(outputImage, RIGHT_IMAGE);
+        inputImage = outputImage; // update the input
     }
 }
